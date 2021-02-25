f4330bd427043335226538475bfce0a57ebb5262
Sarghe-Andrei-Vlad <sargheandreivlad@gmail.com>
2021-02-25 17:32:26 +0200

    Create Global.h

---
+++ Laborator2/Lab2P2/Global.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "student.h"
+
+int compareName(Student a, Student b);
+int compareMathGrade(Student a, Student b);
+int compareEngGrade(Student a, Student b);
+int compareHistGrade(Student a, Student b);
+int compareAvgGrade(Student a, Student b);
